8f9bd87f7681a03e586c7be0f21bfb10ff263597
Sven Willner <sven.willner@gmail.com>
2021-05-27 20:09:50 +0200

    Update email

--- highlight-pointer.c
+++ highlight-pointer.c
@@ -6,7 +6,7 @@
 
   MIT License
 
-  Copyright (c) 2020 Sven Willner <sven.willner@gmail.com>
+  Copyright (c) 2020 Sven Willner <sven.willner@yfx.de>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
